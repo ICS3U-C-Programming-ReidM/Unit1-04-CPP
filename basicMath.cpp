c453beded854376d96ae1c083b06a2cb69bf9efa
Reid MacLean <reid.maclean@stu.ocsb.ca>
2025-02-17 20:53:15 +0000

    Updated basicMath.cpp with new functions

---
+++ basicMath.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2025 Reid MacLean All rights reserved.
+//
+// Created by Reid MacLean
+// Created on February 2025
+// This program does basic math
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "9+2=" << (9+2) << std::endl;
+    std::cout << "7-2=" << (7-2) << std::endl;
+    std::cout << "4/2=" << (4/2) << std::endl;
+    std::cout << "4+4/2=" << (4+4/2) << std::endl;
+    std::cout << "3+4*2=" << (3+4*2) << std::endl;
+    std::cout << "5+pow(2,3)=" << (5+pow(2,3)) << std::endl;
+    std::cout << "9+2=" << (9 + 2) << std::endl;
+    std::cout << "sqrt(25)" << sqrt(25) << std::endl;
+}
